2b5e7c6ee707682b3e9c84adb89679ca25278471
Joni Karvonen <n9kajo01@students.oamk.fi>
2021-03-24 12:03:36 +0200

    kategoriat

--- luontilauseet.sql
+++ luontilauseet.sql
@@ -9,8 +9,15 @@ id int AUTO_INCREMENT PRIMARY KEY,
 tuotenimi varchar(32),
 hinta decimal(8),
 tuotetiivistelmä text,
-tuotekuvaus varchar(255),
+tuotekuvaus text,
+kuva varchar(255),
 pvm timestamp
+);
+
+CREATE TABLE kategoria(
+id int,
+kategoria varchar(255),
+FOREIGN KEY (id) REFERENCES tuote(id)
 );
 
 insert into tuote(tuotenimi, hinta, tuotekuvaus)
@@ -19,4 +26,12 @@ values
 ("AMD Ryzen 7 5800x", 480, "AMD prosessori"),
 ("AMD Ryzen 5 3600", 220, "AMD prosessori"),
 ("Nvidia GeForce RTX 3090", 2800, "Nvidia näytönohjain"),
-("Samsung 860 EVO SSD", 130, "Samsung ssd")
+("Samsung 860 EVO SSD", 130, "Samsung ssd");
+
+insert into kategoria()
+values
+(1, "Prosessorit"),
+(2, "Prosessorit"),
+(3, "Prosessorit"),
+(4, "Näytönohjaimet"),
+(5, "Kovalevyt")
