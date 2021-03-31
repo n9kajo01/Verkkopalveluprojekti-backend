1a904b53fa201a8c72bb81ce2b0112b0b285a1e0
JoonaHaataja <joona00@windowslive.com>
2021-04-01 01:01:32 +0300

    pieniä muutoksia luontilauseisiin + search.php hakee myös tuotetiivistelmän

--- search.php
+++ search.php
@@ -12,7 +12,7 @@ $input = json_decode(file_get_contents("php://input"));
 $search = filter_var($input->search, FILTER_SANITIZE_STRING);
 $sort = filter_var($input->sort, FILTER_SANITIZE_STRING);
 
-$sql = $db->prepare("SELECT tuotenimi, hinta, tuotekuvaus,id,kuva FROM tuote WHERE tuotenimi like '%$search%' or tuotekuvaus like '%$search%' $sort");
+$sql = $db->prepare("SELECT tuotenimi, hinta, tuotetiivistelmä,tuotekuvaus,id,kuva FROM tuote WHERE tuotenimi like '%$search%' or tuotekuvaus like '%$search%' $sort");
 
 $sql->execute();
 
