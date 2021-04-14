0e72413db5e1782b299fc20c17c210b984836f59
JoonaHaataja <joona00@windowslive.com>
2021-04-14 10:56:05 +0300

    tilatut tuotteet tili sivulle

---
+++ orderedproducts.php
@@ -0,0 +1,24 @@
+<?php
+require_once "headers.php";
+require_once "connection.php";
+
+
+$db = openDb();
+
+$input = json_decode(file_get_contents("php://input"));
+$search = filter_var($input->search, FILTER_SANITIZE_STRING);
+$sql = "
+SELECT *
+FROM tilausrivi
+INNER JOIN tuote
+ON tilausrivi.tuotenro = tuote.id
+WHERE tilausid = '$search'"
+;
+
+
+$query = $db->prepare($sql);
+$query->execute();
+
+$result = $query->fetchAll(PDO::FETCH_ASSOC);
+echo header("HTTP/1.1 200 OK");
+echo json_encode($result);
