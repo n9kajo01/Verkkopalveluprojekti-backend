621539703590d0c8a4c704217a173207ba3810cc
Joni Karvonen <n9kajo01@students.oamk.fi>
2021-04-22 13:44:28 +0300

    register

--- getUser.php
+++ getUser.php
@@ -2,11 +2,21 @@
 session_start();
 require_once "headers.php";
 
-$user = (array)$_SESSION["user"];
-
-$data = array(
-    "userid" => $user["userid"],
-    "email" => $user["email"],
-    "oikeudet" => $user["oikeudet"]
-);
-echo json_encode($data);
+//palauttaa kirjautuneen käyttäjän tiedot jos sivu ladataan uudestaan
+if($_SESSION["user"]){
+    $user = (array)$_SESSION["user"];
+    $data = array(
+        "userid" => $user["userid"],
+        "email" => $user["email"],
+        "etunimi" => $user["etunimi"],
+        "sukunimi" => $user["sukunimi"],
+        "osoite" => $user["osoite"],
+        "postinro" => $user["postinro"],
+        "kunta" => $user["kunta"],
+        "puh" => $user["puh"],
+        "added" => $user["added"],
+        "oikeudet" => $user["oikeudet"]
+    );
+    echo json_encode($data);
+    
+}
